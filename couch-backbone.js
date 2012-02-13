d7a4b0c3a5de5d4a6af5925900f17619429de525
Paul <somethingitalian@gmail.com>
2012-02-13 13:48:08 -0500

    lots of changes to make collections and models work with rewriting and saving models without IDS

--- couch-backbone.js
+++ couch-backbone.js
@@ -1,17 +1,41 @@
 var couchDoc = Backbone.Model.extend(
     {
-	idAttribute: "_id",
+        idAttribute: "_id",
+        urlRoot:function(){
+	    if(_.isEmpty(this.db)){
+                return window.location.href+"api/";  //this almost may not work with a router
+	    }
+	    else{
+		return window.location.protocol + "//" + window.location.hostname + ":" +window.location.port + "/" + this.db;
+	    }
+	},
+	parse:function(resp, xhr) {
+	    //xhr.statusText: "Created"
+	    //resp -> "{"ok":true,"id":"80e3124756c1e5241a6665b683003c86","rev":"1-a013d28d19cf9ffc14b6a76f596fb7d4"}"
+	    if(xhr && xhr.statusText == "Created"){ //this seems to take care of creating and editing docs, i'm not sure if this is the right solution, though
+		return {_rev:resp.rev,_id:resp.id};
+	    }
+	    return resp;
+	},
 	save:function(attrs,options){
-	    function updateRev(model,resp,status){
-		model.set({_rev:status.rev,_id:status.id},{silent: true});
-		resp = _.removeKeys(resp,['id','rev','ok']); //clean resp so that it doesn't pollute out object
-	    };
 	    options || (options = {});
 	    var model = this;
-	    var success = options.success;   
+	    var success = options.success;
+	    if(_.isUndefined(this.id)){
+		//get uuid from couchdb and set the model's id to it, this may not work properly with many requests in a short time span
+		var id = $.couch.newUUID(); //fixme: test this more on firefox, sometimes gives back previous UUIDs
+		var idObj = {};
+		idObj[this.idAttribute] = id;
+		this.set(idObj,{silent:true});
+	    }
 	    options.success = function(resp, status, xhr){
-		updateRev(model,resp,status);
-		if (success){success(model, resp, xhr);}
+		if (success){success(resp, status, xhr);}
+	    };
+	    var error = options.error;   
+	    options.error = function(model, status, xhr){
+		if(status.responseText == '{"error":"not_found","reason":"no_db_file"}'){
+		}
+		if (error){error(model, status, xhr);}
 	    };
 	    Backbone.Model.prototype.save.call(this, attrs, options);
 	},	
@@ -22,18 +46,20 @@ var couchDoc = Backbone.Model.extend(
 	    Backbone.Model.prototype.destroy.call(this, options);
 	}
     });
+
 /*
  couch = {db:dbname} they are names referring to the names of the database and ddoc
  options =  what you would normally use in .extend()... if you overwrite url,parse, or other things that couchCollection use it will probably not work right
  */
 var couchCollection = function(couch,options){
     couch || (couch = {});
-    couch.db || (couch.db = 'db');
+    //couch.db || (couch.db = 'db');
     options || (options = {});
-    var urlBase = window.location.protocol + "//" + window.location.hostname + ":" +window.location.port + "/";
+    var urlBase = window.location.protocol + "//" + window.location.hostname + ":" +window.location.port;// + "/";
     return Backbone.Collection.extend(
 	_.extend(options,
-		 {url:urlBase + couch.db,
+		 {
+		  url: (couch.db)? urlBase+ "/_rewrite/"+couch.db+"/" : urlBase + "/_rewrite/api/",// + couch.db, //FIXME: this has got to change
 		  parse: function(response) {
 		      return _(response.rows)
 			  .chain()
@@ -43,45 +69,8 @@ var couchCollection = function(couch,options){
 		  },
 		  fetch:function(options){
 		      options || (options = {});
-		      var fetch_options = _.extend(options,{url:this.url+"/_all_docs", data:{include_docs:true}});
+		      var fetch_options = _.extend(options,{url:this.url +"_all_docs", data:{include_docs:true}});
 		      Backbone.Collection.prototype.fetch.call(this,fetch_options);
 		  }
 		 }));
 };
-
-
-//-------------------- not part of couch-backbone yet------------------//
-//this is an outbound parser//
-
-//testing for persistfilter
-/*
-Backbone.Model.prototype.save = function() {
-  var _save = Backbone.Model.prototype.save;
-  
-  return function(attrs, options) {
-    attrs = _.clone(attrs);
-
-    if (typeof this.persistFilter === "function") {
-      attrs = this.persistFilter(attrs) || attrs;
-    }
-
-    _save.call(this, attrs, options);
-  };
-}();
-
-var Test = Backbone.Model.extend({
-  initialize: function() {
-    this.set({ lol: "HI" });
-  },
-
-  persistFilter: function(attrs) {
-    var newAttrs = {};
-
-    _.each(attrs, function(val, key) {
-      newAttrs["prefix::" + key] = val;
-    });
-
-    return newAttrs;
-  }
-});
-*/
